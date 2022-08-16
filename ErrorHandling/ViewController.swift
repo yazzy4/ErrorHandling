aa1e22b57c07b0ef3bfa1d549389da4d19254405
Yaz Burrell <yazburrell@Yazs-MacBook-Pro.local>
2022-08-16 14:07:06 -0400

    added easy encryption method for do catch error handling

--- ErrorHandling/ViewController.swift
+++ ErrorHandling/ViewController.swift
@@ -7,12 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum EncryptionError: Error {
+    case empty
+    case short
+}
 
+class ViewController: UIViewController {
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // do this only if the condition passes
+        do {
+            let encrypted = try encrypt("hackable, secret info", withPassword: "123456")
+            print(encrypted)
+        //but just in case
+        } catch EncryptionError.empty {
+            print("You must provide a password")
+        } catch EncryptionError.short {
+            print("Passwords must be at least 5 characters, preferably 8 with a special character and number")
+        } catch {
+            print("Something ain't right")
+        }
     }
+
+    // Method to encrypt return an 'encrypted' pw
+    // Adding throws protects against unacceptable pw conditions e.g., a pw called pw or no pw
+    func encrypt(_ str: String, withPassword password: String) throws -> String {
+        guard password.count > 0 else { throw EncryptionError.empty }
+        guard password.count >= 4 else { throw EncryptionError.short }
+        
+        let encrypted = password + str + password
+        
+        return String(encrypted.reversed())
+    }
+    
+    
+    
 
 
 }
