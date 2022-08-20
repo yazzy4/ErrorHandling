9c3ebced5c9fbb5d74ee6da44c7f91feeca7d0cc
Yaz Burrell <yazburrell@Yazs-MacBook-Pro.local>
2022-08-20 10:15:22 -0400

    Added result and non result methods tfor basic async call

--- ErrorHandling/Result.swift
+++ ErrorHandling/Result.swift
@@ -13,4 +13,31 @@ enum NetworkError: Error {
 }
 
 
+func fetchUnreadCount1(from urlString: String, completionHandler: @escaping (Result<Int, NetworkError>) -> Void) {
+    guard let url = URL(string: urlString) else {
+        completionHandler(.failure(.badURL))
+        return
+    }
+    
+    print("Fetching \(url.absoluteString)....")
+    completionHandler(.success(5))
+}
 
+//fetchUnreadCount1(from: "https://www.hackingwithswift.com") { result in
+//    switch result {
+//    case .success(let count):
+//        print("\(count) unread messages.")
+//    case .failure(let error):
+//        print(error.localizedDescription)
+//    }
+//}
+
+func fetchUnreadCount2(from urlString: String, completionHandler: @escaping(Int?, NetworkError?) -> Void) {
+    guard let url = URL(string: urlString) else {
+        completionHandler(nil, .badURL)
+        return
+    }
+    
+    print("Fetching \(url.absoluteString)...")
+    completionHandler(5, nil)
+}
